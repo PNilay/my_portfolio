6b0d15bb2394c8e9c2d95cf3d852a12ab67888c2
Pnilay <pnilay926@gmail.com>
2023-01-31 00:02:18 -0500

    removed duplicate code and created reusable component to fectch data from json file

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./ProjectPage.css";
 import ProjectCards from "../projectCards/ProjectCards";
 import { AiOutlineGithub } from "react-icons/ai";
-import {AiOutlineGlobal} from "react-icons/ai"
+import { AiOutlineGlobal } from "react-icons/ai";
+
+import data from "./project_list.json";
+
 const Projects = () => {
   return (
     <div className="main_div scroll" id="/projects">
@@ -10,179 +13,45 @@ const Projects = () => {
         <div>projects</div>
       </div>
       <div className="project_container">
-      <div className="box">
-          <div className="child_projct_card">
-            <ProjectCards
-              proj_name="AlgoVisualizer"
-              proj_description="Responsive, interactive and dynamic web-based application to visualize various types of path finding algorithm (BFS, DFS, Dijkstra's and A*) in grid world and maze settings.
-              "
-              prog_languages="Angular, TypeScript, HTML, CSS"
-            />
-          </div>
-          <div className="project_name">
-            <a
-              href="https://github.com/PNilay/algo-visualizer"
-              target="_blank"
-              rel="noreferrer"
-              className="github_link"
-            >
-              <AiOutlineGithub className="github_logo" />
-            </a>
-            <a
-              href="https://pnilay.github.io/algo-visualizer/"
-              target="_blank"
-              rel="noreferrer"
-              className="github_link"
-            >
-              <AiOutlineGlobal className="github_logo" />
-            </a>
-          </div>
-        </div>
-
-        <div className="box">
-          <div className="child_projct_card">
-            <ProjectCards
-              proj_name="LSTM Network From Scratch (Text Generation & Image Classification)"
-              proj_description="Implemented text generation and MNIST handwritten digit classification models using LSTM network from scratch.
-              "
-              prog_languages="Python/Jupyter Notebook"
-            />
-          </div>
-          <div className="project_name">
-            <a
-              href="https://github.com/PNilay/LSTM-Network-from-Scratch"
-              target="_blank"
-              rel="noreferrer"
-              className="github_link"
-            >
-              <AiOutlineGithub className="github_logo" />
-            </a>
-          </div>
-        </div>
-
-
-        <div className="box">
-          <div className="child_projct_card">
-            <ProjectCards
-              proj_name="Travel Reservation System"
-              proj_description="Collaborated in team of four students to design and implement database system to support the operation of an online travel
-              reservation system
-              "
-              prog_languages="HTML MySql CSS JavaScript JSP"
-            />
-          </div>
-          <div className="project_name">
-            <a
-              href="https://github.com/micort35/travel_db"
-              target="_blank"
-              rel="noreferrer"
-              className="github_link"
-            >
-              <AiOutlineGithub className="github_logo" />
-            </a>
-          </div>
-        </div>
-        <div className="box">
-          <div className="child_projct_card">
-            <ProjectCards
-              proj_name="Where’s the File"
-              proj_description="Version control system similar to Git is implemented in this project
-              "
-              prog_languages="C"
-            />
-          </div>
-          <div className="project_name">
-            {" "}
-            <a
-              href="https://github.com/PNilay/versioncontrol-system"
-              target="_blank"
-              rel="noreferrer"
-              className="github_link"
-            >
-              <AiOutlineGithub className="github_logo" />
-            </a>
-          </div>
-        </div>
-        <div className="box">
-          <div className="child_projct_card">
-            <ProjectCards
-              proj_name="DNS Socket Programming Project"
-              proj_description="Implemented Domain Name System (DNS) with authentication server to get the ip adress given a host name of the server"
-              prog_languages="Python"
-            />
-          </div>
-          <div className="project_name">
-            {" "}
-            <a
-              href="https://github.com/PNilay/DNS_Socket_Programming_Project"
-              target="_blank"
-              rel="noreferrer"
-              className="github_link"
-            >
-              <AiOutlineGithub className="github_logo" />
-            </a>
-          </div>
-        </div>
-        <div className="box">
-          <div className="child_projct_card">
-            <ProjectCards
-              proj_name="Bank and Stock Management System"
-              proj_description="This project models the simple stock market and the bank account using the c++ class features"
-              prog_languages="C++"
-            />
-          </div>
-          <div className="project_name">
-            {" "}
-            <a
-              href="https://github.com/PNilay/Bank_and_Stock_Management_System"
-              target="_blank"
-              rel="noreferrer"
-              className="github_link"
-            >
-              <AiOutlineGithub className="github_logo" />
-            </a>
-          </div>
-        </div>
-        <div className="box">
-          <div className="child_projct_card">
-            <ProjectCards
-              proj_name="Fast Trajectory Re-planning"
-              proj_description="Implemented multiple types of A* algorithm to find path from start to target"
-              prog_languages="Java"
-            />
-          </div>
-          <div className="project_name">
-            {" "}
-            <a
-              href="https://github.com/PNilay/Trejectory_Replanning"
-              target="_blank"
-              rel="noreferrer"
-              className="github_link"
-            >
-              <AiOutlineGithub className="github_logo" />
-            </a>
-          </div>
-        </div>
-        <div className="box">
-          <div className="child_projct_card">
-            <ProjectCards
-              proj_name="Handwritten Digits Recognition Using Fully Conneted Neural Network"
-              proj_description="Implemented a fully connected neural network on MNIST handwritten digits dataset using only NumPy to correctly classify each image"
-              prog_languages="Python"
-            />
-          </div>
-          <div className="project_name">
-            {" "}
-            <a
-              href="https://github.com/PNilay/FullyConnected_NN_numpy"
-              target="_blank"
-              rel="noreferrer"
-              className="github_link"
-            >
-              <AiOutlineGithub className="github_logo" />
-            </a>
-          </div>
-        </div>
+        {data.map((project, i) => {
+          return (
+            <div className="box">
+              <div className="child_projct_card">
+                <ProjectCards
+                  proj_name={project.proj_name}
+                  proj_description={project.proj_description}
+                  prog_languages={project.proj_languages}
+                />
+              </div>
+              <div className="project_name">
+                {project.proj_github != "" ? (
+                  <a
+                    href={project.proj_github}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="github_link"
+                  >
+                    <AiOutlineGithub className="github_logo" />
+                  </a>
+                ) : (
+                  ""
+                )}
+                {project.proj_website != "" ? (
+                  <a
+                    href={project.proj_website}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="github_link"
+                  >
+                    <AiOutlineGlobal className="github_logo" />
+                  </a>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
