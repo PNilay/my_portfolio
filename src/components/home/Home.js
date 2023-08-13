35f1e332e39304f39316fc7fe81071647b63304b
Pnilay <pnilay926@gmail.com>
2023-08-13 15:01:20 -0400

    updated conetent on home screen

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
   return (
       <div className="HomePage_info" id="/">
         <div className="homepage_container">
-          <div className="user_name_heading">Hello, I'm Nilay Patel.</div>
-          <div className="info_heading">I am an Engineer.</div>
+          <div className="user_name_heading">Turning Vision into Pixels.</div>
+          {/* <div className="info_heading">I am an Engineer.</div> */}
           {/* <img src="../Group 1.svg"alt="Girl in a jacket" ></img> */}
         </div>
       </div>
