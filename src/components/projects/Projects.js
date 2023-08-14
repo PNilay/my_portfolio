c5370309c835dcfc6f584bf39445339adf531b90
Pnilay <pnilay926@gmail.com>
2023-08-14 09:03:48 -0400

    added option to open carousel from project card

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./ProjectPage.css";
 import ProjectCards from "../projectCards/ProjectCards";
 import { AiOutlineGithub } from "react-icons/ai";
-import { AiOutlineGlobal } from "react-icons/ai";
+import { AiOutlineGlobal, AiFillPicture} from "react-icons/ai";
 
 import data from "./project_list.json";
+import Carousel from "../carousel/Carousel";
+
 
 const Projects = () => {
   return (
     <div className="main_div scroll" id="/projects">
-      <div className="title_bar">
-        <div>projects</div>
+      <div className="title_bar_proj">
+        <div className="proj_title">Dreams in Pixels: A Portfolio Odyssey</div>
       </div>
       <div className="project_container">
         {data.map((project, i) => {
@@ -36,7 +38,20 @@ const Projects = () => {
                 ) : (
                   ""
                 )}
-                {project.proj_website != "" ? (
+                {project.proj_imgs != "" ? (
+                  <a
+                    href={project.proj_imgs}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="github_link"
+                  >
+                    <AiFillPicture className="github_logo" />
+                  </a>
+                ) : (
+                  ""
+                )}
+
+                  {project.proj_imgs != "" ? (
                   <a
                     href={project.proj_website}
                     target="_blank"
