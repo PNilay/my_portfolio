7c173c44d0824f6cc4074ca8e6d000dd3788d705
Pnilay <pnilay926@gmail.com>
2023-08-14 09:02:15 -0400

    updated content on about me section

--- src/components/aboutme/Aboutme.js
+++ src/components/aboutme/Aboutme.js
@@ -3,63 +3,69 @@ import "./aboutme.css";
 const Aboutme = () => {
   return (
     <div className="aboutme_page" id="/aboutme">
-      <div className="title_bar">
-        <div className="aboutme_title">About Me</div>
+      <div className="title_bar_about">
+        <div className="aboutme_title">From Vision to Pixels: My Story</div>
       </div>
 
-      <div className="aboutme_information">
-        <div className="aboutme_bio">
-          <div className="aboutme_content_header">BIO</div>
-          <div className="aboutme_content">
-            Hello, I am Nilay, a highly motivated engineering professional with
-            broad knowledge of academic discipline ranging from circuit
-            designing to web-development. Technology savvy and familiar with
-            many modern systems, software applications as well as various
-            programming languages. I enjoy writing clear & concise code and
-            building things to make a meaningful impact on the world. I have
-            worked on different areas of projects with different programming
-            languages to enhance my skills and knowledge.
-          </div>
+      <div className="aboutme_info">
+        <div className="bio_col">
+          I'm Nilay Patel, a driven engineering professional fueled by a
+          profound passion for transforming ideas into tangible digital
+          realities. My journey spans an extensive spectrum, from intricate
+          circuit design to the realm of dynamic web development. A relentless
+          explorer of technology's frontier, I'm well-acquainted with modern
+          systems, software applications, and a diverse array of programming
+          languages.
+          <br></br> <br /> I derive immense satisfaction from the art of
+          crafting code that is not just functional, but elegant in its clarity.
+          Every keystroke is an opportunity to shape innovation and leave an
+          indelible mark on the world.
         </div>
-        <div className="aboutme_education">
-          <div className="aboutme_content_header">EDUCATION</div>
-          <div className="aboutme_content">
-            <div className="University_name">
-              <a
-                href="https://www.rutgers.edu/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Rutgers University, New Brunswick, New Jersey
-              </a>
-            </div>
-            <div className="University_major">
-              B.S. in Electrical and Computer Engineering & Computer Science
-              (Double Major)
-            </div>
-            <div className="University_gradyear_gpa">May 2020, GPA 3.54</div>
-          </div>
+
+        <div className="bio_col">
+          My voyage has led me through a tapestry of projects, each an
+          opportunity to embrace new challenges and grasp the nuances of various
+          programming languages. This multidimensional approach has been
+          instrumental in honing my skills and expanding my knowledge base.
+          <br></br>
+          <br />
+          With a fervor for the convergence of creativity and functionality, I
+          revel in the process of turning visions into pixels, breathing life
+          into concepts that resonate with users. My commitment is unwavering,
+          my curiosity insatiable, and my dedication resolute. The pixels I
+          sculpt are more than just code; they are gateways to experiences,
+          connections, and change.
+          <br></br>
+          <br />
         </div>
-        <div className="aboutme_skills">
-          <div className="aboutme_content_header">TECHNICAL SKILLS</div>
-          <div className="aboutme_content">
-            <div>
-              <b>Programming Languages</b>: C, C++, Java, Python, MATLAB, HTML,
-              JavaScript, VHDL
-            </div>
-            <div>
-              <b>Operating System/Software</b>: Linux, Windows, MacOS, Microsoft
-              Office, LTspice, Latex, Git, ROS, React
-            </div>
-            <div>
-              <b>Hardware</b>: Arduino, Raspberry Pi, Nvidia Jetson Tx2, Xilinx
-              Zynq-7000 development board
-            </div>
-            <div>
-              {" "}
-              <b>Web Development tools</b>: MySQL, JSP, Sass/CSS, Node.js
-            </div>
+      </div>
+
+      <div className="aboutme_exp">
+        <div className="aboutme_exp_header">Education</div>
+        <div className="aboutme_exp_content">
+          <div className="University_name">
+            <a href="https://www.rutgers.edu/" target="_blank" rel="noreferrer">
+              Rutgers University, New Brunswick, New Jersey
+            </a>
           </div>
+          <div className="University_major">
+            B.S. in Electrical and Computer Engineering & Computer Science
+            (Double Major)
+          </div>
+          <div className="University_gradyear_gpa">May 2020, GPA 3.54</div>
+        </div>
+      </div>
+
+      <div className="aboutme_exp">
+        <div className="aboutme_exp_header">EXPERINCE</div>
+        <div className="aboutme_exp_content">
+          <div className="University_name">
+            <a href="https://www.rutgers.edu/" target="_blank" rel="noreferrer">
+              Infosys Limited
+            </a>
+          </div>
+          <div className="University_major">Software Engineer</div>
+          <div className="University_gradyear_gpa">2020-Present</div>
         </div>
       </div>
     </div>
